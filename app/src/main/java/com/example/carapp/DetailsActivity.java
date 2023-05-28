6f4d1692ea87ca8fa07e996a8e67d91c66a4a3d8
Ammaar <amar290@aucklanduni.ac.nz>
2023-05-28 22:17:17 +1200

    finish xml design of detailsActivity

--- app/src/main/java/com/example/carapp/DetailsActivity.java
+++ app/src/main/java/com/example/carapp/DetailsActivity.java
@@ -3,6 +3,14 @@ package com.example.carapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -10,5 +18,22 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        ImageSlider imageSlider = findViewById(R.id.detailsActivityimageslider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.convertible, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.sedan, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.suv, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.coupe, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
+        TextView textView4 = (TextView) findViewById(R.id.textView4);
+
+        View divider = (View) findViewById(R.id.divider2);
     }
 }
